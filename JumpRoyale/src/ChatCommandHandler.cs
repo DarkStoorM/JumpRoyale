be143cb47bab686c8e46ed642b0fe48b9619791a
darkstoorm <b.sieprawski@op.pl>
2024-02-10 00:59:02 +0100

    Initial commit

---
+++ JumpRoyale/src/ChatCommandHandler.cs
@@ -0,0 +1,5 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Godot;
+
+public class CommandHandler(string message, string senderId, string senderName, string hexColor, bool isPrivileged) { }
