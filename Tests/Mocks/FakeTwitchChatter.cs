3d1bef536921d99353febd004f7d116d992c817a
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-02-20 20:30:11 +0100

    Update fake chatter
    - removes Message requirement - this is handled inside tests anyway
    - adds setters - we should be able to do anything we want with a mocked instance

--- Tests/Mocks/FakeTwitchChatter.cs
+++ Tests/Mocks/FakeTwitchChatter.cs
@@ -2,18 +2,11 @@ using JumpRoyale.Utils;
 
 namespace Tests.Mocks;
 
-public class FakeTwitchChatter(
-    string message,
-    string? displayName = null,
-    string? userId = null,
-    string? colorHex = null
-)
+public class FakeTwitchChatter(string? displayName = null, string? userId = null, string? colorHex = null)
 {
-    public string ColorHex { get; } = colorHex ?? Rng.RandomHex();
+    public string ColorHex { get; set; } = colorHex ?? Rng.RandomHex();
 
-    public string DisplayName { get; } = displayName ?? Path.GetRandomFileName();
+    public string DisplayName { get; set; } = displayName ?? Path.GetRandomFileName();
 
-    public string Message { get; set; } = message;
-
-    public string UserId { get; } = userId ?? Rng.RandomInt().ToString();
+    public string UserId { get; set; } = userId ?? Rng.RandomInt().ToString();
 }
