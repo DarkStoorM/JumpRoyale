11f98619b2682087b5a4e02def194b6ef2749ba6
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-02-24 18:55:40 +0100

    Add base for Arena with a dummy Join helper

--- JumpRoyale/src/ArenaScene.cs
+++ JumpRoyale/src/ArenaScene.cs
@@ -1,4 +1,8 @@
 using Godot;
+using JumpRoyale.Commands;
+using JumpRoyale.Events;
+using TwitchChat;
+using TwitchChat.Extensions;
 
 namespace JumpRoyale;
 
@@ -6,6 +10,57 @@ public partial class ArenaScene : Node2D
 {
     public override void _Ready()
     {
-        // Placeholder
+        TwitchChatClient.Initialize(new());
+        PlayerStats.Initialize(ProjectSettings.GlobalizePath("res://save_data/players.json"));
+
+        TwitchChatClient.Instance.OnTwitchMessageReceivedEvent += OnMessageReceived;
+        PlayerStats.Instance.OnPlayerJoin += OnPlayerJoin;
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (Input.IsPhysicalKeyPressed(Key.J))
+        {
+            TwitchChatClient.Instance.InvokeFakeMessageEvent("join");
+        }
+    }
+
+    /// <summary>
+    /// Action automatically called when a new event is raised by Twitch Chat Client upon receiving a chat message.
+    /// </summary>
+    private void OnMessageReceived(object sender, ChatMessageEventArgs args)
+    {
+        CallDeferred(
+            nameof(HandleChatMessage),
+            args.Message,
+            args.UserId,
+            args.DisplayName,
+            args.ColorHex,
+            args.IsPrivileged
+        );
+    }
+
+    /// <summary>
+    /// Action automatically called when a new event is raised by the PlayerStats upon joining the game.
+    /// </summary>
+    private void OnPlayerJoin(object sender, PlayerJoinEventArgs eventArgs)
+    {
+        // TODO: Spawn a new player here
+    }
+
+    /// <summary>
+    /// Executes internal command if the provided chat message matches any of the aliases for those commands.
+    /// </summary>
+    private void HandleChatMessage(
+        string message,
+        string senderId,
+        string senderName,
+        string hexColor,
+        bool isPrivileged
+    )
+    {
+        ChatCommandHandler commandHandler = new(message, senderId, senderName, hexColor, isPrivileged);
+
+        commandHandler.ProcessMessage();
     }
 }
