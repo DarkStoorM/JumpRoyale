375abbddb05ac90709a842b48671f835fa262189
darkstoorm <b.sieprawski@op.pl>
2024-02-27 13:24:46 +0100

    Add missing docs

--- JumpRoyale/src/Utils/GameConstants.cs
+++ JumpRoyale/src/Utils/GameConstants.cs
@@ -2,6 +2,9 @@ using Godot;
 
 namespace JumpRoyale;
 
+/// <summary>
+/// Class providing constants serving as values for all game components.
+/// </summary>
 public static class GameConstants
 {
     public static readonly float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
