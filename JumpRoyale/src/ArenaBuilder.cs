85d78bb337bafeb4c5bc871f216a9163bc5d8188
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-02-29 20:56:00 +0100

    Type updates, add docs, store platforms in dictionary, add initial platform drawing

--- JumpRoyale/src/ArenaBuilder.cs
+++ JumpRoyale/src/ArenaBuilder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 using JumpRoyale.Utils;
 
@@ -11,9 +12,18 @@ public class ArenaBuilder
 
     private readonly TileSet _tileSet;
 
+    /// <summary>
+    /// Collection of all Platforms that can be used for drawing one-way collision platforms.
+    /// </summary>
+    private readonly Dictionary<GameTileTypes, BasePlatform> _platforms = [];
+
     private ArenaBuilder(TileSet tileSet)
     {
         _tileSet = tileSet;
+
+        _platforms.Add(GameTileTypes.PlatformConcrete, GameTiles.PlatformConcrete);
+        _platforms.Add(GameTileTypes.PlatformGold, GameTiles.PlatformGold);
+        _platforms.Add(GameTileTypes.PlatformStone, GameTiles.PlatformStone);
     }
 
     public static ArenaBuilder Instance
@@ -32,6 +42,11 @@ public class ArenaBuilder
         }
     }
 
+    /// <summary>
+    /// Creates ArenaBuilder instance with TileSet containing sprites to assign to platforms and other objects (walls,
+    /// etc.).
+    /// </summary>
+    /// <param name="tileSet">TileSet with atlas that should include all sprites.</param>
     public static void Initialize(TileSet tileSet)
     {
         NullGuard.ThrowIfNull(tileSet);
@@ -42,28 +57,55 @@ public class ArenaBuilder
         }
     }
 
-    public void DrawPoint(Vector2 location)
+    /// <summary>
+    /// Draws a new platform on the arena in specified location.
+    /// </summary>
+    /// <remarks>
+    /// The shortest platform is 2 tiles long, which will always draw Left and Right. <c>length</c> parameter specifies
+    /// how many additional tiles to draw, which extend the platform.
+    /// </remarks>
+    /// <param name="location">Starting point where the platform is drawn (starting from Left).</param>
+    /// <param name="length">How many Middle tiles to insert.</param>
+    /// <param name="drawWith">Type of the platform to draw.</param>
+    public void DrawPlatform(Vector2I location, int length, GameTileTypes drawWith = GameTileTypes.PlatformStone)
+    {
+        // Retrieves the default platform for drawing unless specified otherwise
+        // BasePlatform platform = _platforms[drawWith];
+        int start = location.X;
+        int end = location.X + length;
+
+        for (int x = start; start < end; x++)
+        {
+            // TBA
+        }
+    }
+
+    /// <summary>
+    /// Draws a single sprite on the TileMap at given location.
+    /// </summary>
+    /// <param name="location">Coordinates (location) on TileMap to draw at.</param>
+    public void DrawPoint(Vector2I location)
     {
         // TBA
         GD.Print(_tileSet);
     }
 
-    public void DrawLineHorizontally(Vector2 location, int length)
+    /// <summary>
+    /// Draws a set of sprites on the TileMap as a horizontal line of specified length.
+    /// </summary>
+    /// <param name="location">Starting point where the line should be drawn from.</param>
+    /// <param name="length">Length of the line to draw. If the length is 0, only Left+Right is drawn.</param>
+    public void DrawLineHorizontally(Vector2I location, int length)
     {
         // TBA
     }
 
-    public void DrawLineVertically(Vector2 location, int length)
+    public void DrawSquare(Vector2I location, int size, bool shouldFill = false)
     {
         // TBA
     }
 
-    public void DrawSquare(Vector2 location, int size, bool shouldFill = false)
-    {
-        // TBA
-    }
-
-    public void DrawRectangle(Vector2 startingPoint, Vector2 endingPoint, bool shouldFill = false)
+    public void DrawRectangle(Vector2I startingPoint, Vector2I endingPoint, bool shouldFill = false)
     {
         // TBA
     }
