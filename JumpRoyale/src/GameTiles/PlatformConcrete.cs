fbf48765cc3de39a764b1cb12117d3fdad73adb9
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-02-29 20:54:29 +0100

    Add game platforms that can be drawn (spawned)

---
+++ JumpRoyale/src/GameTiles/PlatformConcrete.cs
@@ -0,0 +1,5 @@
+using Godot;
+
+namespace JumpRoyale;
+
+public class PlatformConcrete(Vector2I left, Vector2I middle, Vector2I right) : BasePlatform(left, middle, right) { }
---
+++ JumpRoyale/src/GameTiles/PlatformGold.cs
@@ -0,0 +1,5 @@
+using Godot;
+
+namespace JumpRoyale;
+
+public class PlatformGold(Vector2I left, Vector2I middle, Vector2I right) : BasePlatform(left, middle, right) { }
