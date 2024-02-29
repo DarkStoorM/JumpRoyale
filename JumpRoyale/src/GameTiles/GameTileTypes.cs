9d5e88a7f6d6c7105c1bafc5739e7a61327d180a
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-02-29 20:53:56 +0100

    Add types of platforms for Dictionary lookup

---
+++ JumpRoyale/src/GameTiles/GameTileTypes.cs
@@ -0,0 +1,11 @@
+namespace JumpRoyale;
+
+/// <summary>
+/// Defines the types of tiles or platforms for Drawing.
+/// </summary>
+public enum GameTileTypes
+{
+    PlatformConcrete,
+    PlatformGold,
+    PlatformStone,
+}
