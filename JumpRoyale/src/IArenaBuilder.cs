786d81342388bf118b90ca754cff0dc67ca53fb7
darkstoorm <b.sieprawski@op.pl>
2024-03-03 18:33:57 +0100

    Update docs on `drawWith`

--- JumpRoyale/src/IArenaBuilder.cs
+++ JumpRoyale/src/IArenaBuilder.cs
@@ -19,7 +19,8 @@ public interface IArenaBuilder
     /// </remarks>
     /// <param name="location">Starting point where the platform is drawn (starting from Left).</param>
     /// <param name="length">How many Middle tiles to insert.</param>
-    /// <param name="drawWith">Type of the platform to draw.</param>
+    /// <param name="drawWith">Type of the platform to draw. This parameter does not have to be specified in order to
+    /// draw with the default <c>Stone</c> tile. Override this if you need to draw with a different tile.</param>
     public void DrawHorizontalPlatform(Vector2I location, int length, TileTypes drawWith = TileTypes.Stone);
 
     /// <summary>
@@ -37,7 +38,8 @@ public interface IArenaBuilder
     /// </remarks>
     /// <param name="location">Starting point where the wall is drawn (starting from Left).</param>
     /// <param name="length">How many Middle tiles to insert.</param>
-    /// <param name="drawWith">Type of the wall to draw.</param>
+    /// <param name="drawWith">Type of the wall to draw. This parameter does not have to be specified in order to
+    /// draw with the default <c>Stone</c> tile. Override this if you need to draw with a different tile.</param>
     public void DrawHorizontalWall(Vector2I location, int length, TileTypes drawWith = TileTypes.Stone);
 
     /// <summary>
@@ -55,13 +57,16 @@ public interface IArenaBuilder
     /// </remarks>
     /// <param name="location">Starting point where the wall is drawn (starting from Top).</param>
     /// <param name="height">How many Middle tiles to insert.</param>
-    /// <param name="drawWith">Type of the wall to draw.</param>
+    /// <param name="drawWith">Type of the wall to draw. This parameter does not have to be specified in order to
+    /// draw with the default <c>Stone</c> tile. Override this if you need to draw with a different tile.</param>
     public void DrawVerticalWall(Vector2I location, int height, TileTypes drawWith = TileTypes.Stone);
 
     /// <summary>
     /// Draws a single sprite on the TileMap at given location.
     /// </summary>
     /// <param name="location">Coordinates on TileMap to draw at.</param>
+    /// <param name="drawWith">Type of the wall to draw. This parameter does not have to be specified in order to
+    /// draw with the default <c>Stone</c> tile. Override this if you need to draw with a different tile.</param>
     public void DrawPoint(Vector2I location, TileTypes drawWith = TileTypes.Stone);
 
     /// <summary>
@@ -75,7 +80,9 @@ public interface IArenaBuilder
     /// <param name="location">Starting point where the object is drawn from.</param>
     /// <param name="size">Amount of units this square will be expanded diagonally for.</param>
     /// <param name="shouldFill">If true, the object will be filled with sprites (default or manually selected).</param>
-    /// <param name="drawWith">Primary object to draw the bounds with.</param>
+    /// <param name="drawWith">Primary object to draw the bounds with. This parameter does not have to be specified in
+    /// order to draw with the default <c>Stone</c> tile. Override this if you need to draw with a different
+    /// tile.</param>
     /// <param name="fillWith">Secondary object used as a filler. <c>drawWith</c> is used as default if none was
     /// specified and if <c>shouldFill</c> was true.</param>
     public void DrawSquare(
@@ -96,7 +103,9 @@ public interface IArenaBuilder
     /// <param name="startingPoint">Location of the bottom-left corner to start drawing from.</param>
     /// <param name="endingPoint">Location of the top-right corner to end the drawing at.</param>
     /// <param name="shouldFill">If true, the object will be filled with sprites (default or manually selected).</param>
-    /// <param name="drawWith">Primary object to draw the bounds with.</param>
+    /// <param name="drawWith">Primary object to draw the bounds with. This parameter does not have to be specified in
+    /// order to draw with the default <c>Stone</c> tile. Override this if you need to draw with a different
+    /// tile.</param>
     /// <param name="fillWith">Secondary object used as a filler. <c>drawWith</c> is used as default if none was
     /// specified and if <c>shouldFill</c> was true.</param>
     public void DrawBox(
