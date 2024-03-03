059702c28dd9dbef437d6b4bd207264e027ab989
darkstoorm <b.sieprawski@op.pl>
2024-03-03 18:34:57 +0100

    Clarify the default tile usage

--- JumpRoyale/src/GameTiles/TileTypes.cs
+++ JumpRoyale/src/GameTiles/TileTypes.cs
@@ -4,5 +4,9 @@ public enum TileTypes
 {
     Concrete,
     Gold,
+
+    /// <summary>
+    /// This is the default tile that is being used for drawing. You don't need to explicitly use this tile.
+    /// </summary>
     Stone,
 }
