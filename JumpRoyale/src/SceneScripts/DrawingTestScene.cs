a0071b4854879262b0ebc75ce842eef520e799f5
darkstoorm <b.sieprawski@op.pl>
2024-03-03 18:58:28 +0100

    Fix positioning, add Rectangle Drawing test

--- JumpRoyale/src/SceneScripts/DrawingTestScene.cs
+++ JumpRoyale/src/SceneScripts/DrawingTestScene.cs
@@ -69,9 +69,9 @@ public partial class DrawingTestScene : Node2D
         _builder.DrawVerticalWall(new Vector2I(16, 16), 1, TileTypes.Concrete);
         _builder.DrawVerticalWall(new Vector2I(17, 16), 2, TileTypes.Concrete);
 
-        _builder.DrawVerticalWall(new Vector2I(21, 16), 0, TileTypes.Gold);
-        _builder.DrawVerticalWall(new Vector2I(22, 16), 1, TileTypes.Gold);
-        _builder.DrawVerticalWall(new Vector2I(23, 16), 2, TileTypes.Gold);
+        _builder.DrawVerticalWall(new Vector2I(20, 16), 0, TileTypes.Gold);
+        _builder.DrawVerticalWall(new Vector2I(21, 16), 1, TileTypes.Gold);
+        _builder.DrawVerticalWall(new Vector2I(22, 16), 2, TileTypes.Gold);
     }
 
     private void DrawTestSquares()
@@ -109,7 +109,7 @@ public partial class DrawingTestScene : Node2D
         _builder.DrawSquare(new Vector2I(24, 38), 3, true, TileTypes.Concrete, TileTypes.Concrete);
         _builder.DrawSquare(new Vector2I(24, 43), 3, true, TileTypes.Gold, TileTypes.Gold);
 
-        // DIfferent filling
+        // Different filling
         _builder.DrawSquare(new Vector2I(29, 30), 0, true, TileTypes.Stone, TileTypes.Stone);
         _builder.DrawSquare(new Vector2I(29, 35), 0, true, TileTypes.Concrete, TileTypes.Concrete);
         _builder.DrawSquare(new Vector2I(29, 40), 0, true, TileTypes.Gold, TileTypes.Gold);
@@ -127,5 +127,56 @@ public partial class DrawingTestScene : Node2D
         _builder.DrawSquare(new Vector2I(38, 43), 3, true, TileTypes.Gold, TileTypes.Stone);
     }
 
-    private void DrawTestRectangles() { }
+    private void DrawTestRectangles()
+    {
+        _builder.DrawBox(new Vector2I(44, 30), new Vector2I(44, 30), drawWith: TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(44, 35), new Vector2I(44, 35), drawWith: TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(44, 40), new Vector2I(44, 40), drawWith: TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(46, 31), new Vector2I(48, 30), drawWith: TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(46, 36), new Vector2I(48, 35), drawWith: TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(46, 41), new Vector2I(48, 40), drawWith: TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(50, 32), new Vector2I(53, 30), drawWith: TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(50, 37), new Vector2I(53, 35), drawWith: TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(50, 42), new Vector2I(53, 40), drawWith: TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(55, 33), new Vector2I(57, 30), drawWith: TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(55, 38), new Vector2I(57, 35), drawWith: TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(55, 43), new Vector2I(57, 40), drawWith: TileTypes.Gold);
+
+        // Filled
+        _builder.DrawBox(new Vector2I(59, 30), new Vector2I(59, 30), true, TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(59, 35), new Vector2I(59, 35), true, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(59, 40), new Vector2I(59, 40), true, TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(61, 31), new Vector2I(63, 30), true, TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(61, 36), new Vector2I(63, 35), true, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(61, 41), new Vector2I(63, 40), true, TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(65, 32), new Vector2I(68, 30), true, TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(65, 37), new Vector2I(68, 35), true, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(65, 42), new Vector2I(68, 40), true, TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(70, 33), new Vector2I(72, 30), true, TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(70, 38), new Vector2I(72, 35), true, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(70, 43), new Vector2I(72, 40), true, TileTypes.Gold);
+
+        // Different filling
+        _builder.DrawBox(new Vector2I(74, 30), new Vector2I(74, 30), true, TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(74, 35), new Vector2I(74, 35), true, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(74, 40), new Vector2I(74, 40), true, TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(76, 31), new Vector2I(78, 30), true, TileTypes.Stone);
+        _builder.DrawBox(new Vector2I(76, 36), new Vector2I(78, 35), true, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(76, 41), new Vector2I(78, 40), true, TileTypes.Gold);
+
+        _builder.DrawBox(new Vector2I(80, 32), new Vector2I(83, 30), true, TileTypes.Stone, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(80, 37), new Vector2I(83, 35), true, TileTypes.Concrete, TileTypes.Gold);
+        _builder.DrawBox(new Vector2I(80, 42), new Vector2I(83, 40), true, TileTypes.Gold, TileTypes.Stone);
+
+        _builder.DrawBox(new Vector2I(85, 33), new Vector2I(87, 30), true, TileTypes.Stone, TileTypes.Concrete);
+        _builder.DrawBox(new Vector2I(85, 38), new Vector2I(87, 35), true, TileTypes.Concrete, TileTypes.Gold);
+        _builder.DrawBox(new Vector2I(85, 43), new Vector2I(87, 40), true, TileTypes.Gold, TileTypes.Stone);
+    }
 }
