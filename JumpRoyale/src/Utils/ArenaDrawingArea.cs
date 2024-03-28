d0b14d7a8a936a4076554c26143c3b10cf7ace42
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-03-28 20:19:40 +0100

    Add a separate class for storing selected Rect2 properties

---
+++ JumpRoyale/src/Utils/ArenaDrawingArea.cs
@@ -0,0 +1,10 @@
+namespace JumpRoyale.Utils;
+
+public class ArenaDrawingArea()
+{
+    public int StartX { get; init; }
+
+    public int EndX { get; init; }
+
+    public int SizeInTiles { get; init; }
+}
