c9251bf272d8f903d3acda61d45ee87d1d039147
darkstoorm <b.sieprawski@op.pl>
2024-04-01 17:15:52 +0200

    Finalize animation handling, rename props, refactor the class

--- JumpRoyale/src/SceneScripts/JumperScene.cs
+++ JumpRoyale/src/SceneScripts/JumperScene.cs
@@ -26,24 +26,37 @@ public partial class JumperScene : CharacterBody2D
     [AllowNull]
     private CpuParticles2D _glow = null!;
 
-    private Vector2 _jumpVelocity;
+    private Vector2 _jumpVelocityFromCommand;
 
     /// <summary>
     /// Previous X Velocity value of this jumper before MoveAndSlide was called. Used to bounce the jumper off the wall.
     /// </summary>
-    private float _previousXVelocity;
+    private float _xVelocityLastFrame;
 
     /// <summary>
-    /// Indicated for how many frames this jumper has been in the same place under certain conditions. See <see
+    /// Indicates for how many frames this jumper has been in the same place under certain conditions. See <see
     /// cref="StorePosition"/>.
     /// </summary>
     private int _framesSincePositionChange;
+
+    private int _lastJumpAngleFromCommand;
+
+    /// <summary>
+    /// Flag defining if we have jumped straight up. Used for the rotating sprite switch and sprite flipping if there
+    /// was a non-zero angle applied.
+    /// </summary>
+    private bool HasRecentlyJumpedAtZeroAngle
+    {
+        get => _lastJumpAngleFromCommand == 90;
+    }
 
     public override void _Ready()
     {
         _animatedSprite2D = GetNode<AnimatedSprite2D>("Sprite2D");
         _namePlate = GetNode<Label>("NamePlate");
         _glow = GetNode<CpuParticles2D>("Glow");
+
+        _animatedSprite2D.AnimationFinished += HandleAnimationFinishEvent;
     }
 
     public void Init(Jumper jumper)
@@ -79,13 +92,12 @@ public partial class JumperScene : CharacterBody2D
         // This order is important: apply gravity -> stop if touched the floor -> then apply the jump velocity
         ApplyInitialVelocity(delta);
         StopIfOnFloor();
+        PlayNotGroundedAnimation();
         ApplyJumpVelocity();
+        RotateInAir(delta);
         BounceOffWall();
 
-        // Remaining from the old code:
-        // Flip the sprite if necessary
-        // Switch the Sprite animation if jumped
-        _previousXVelocity = Velocity.X;
+        _xVelocityLastFrame = Velocity.X;
 
         MoveAndSlide();
         StorePosition();
@@ -116,15 +128,29 @@ public partial class JumperScene : CharacterBody2D
         HandleNameColorEvent();
     }
 
+    /// <summary>
+    /// Switches the animation once the last animation to finish is determined to be "Land".
+    /// </summary>
+    private void HandleAnimationFinishEvent()
+    {
+        if (_animatedSprite2D.Animation == JumperAnimationNames.Land)
+        {
+            _animatedSprite2D.Play(JumperAnimationNames.Idle);
+        }
+    }
+
     private void HandleJumpEvent(int angle, int power)
     {
         if (IsOnFloor())
         {
             double normalizedPower = Math.Sqrt(power * 5 * GameConstants.Gravity);
 
-            _jumpVelocity.X = Mathf.Cos(Mathf.DegToRad(angle + 180));
-            _jumpVelocity.Y = Mathf.Sin(Mathf.DegToRad(angle + 180));
-            _jumpVelocity = _jumpVelocity.Normalized() * (float)normalizedPower;
+            _jumpVelocityFromCommand.X = Mathf.Cos(Mathf.DegToRad(angle + 180));
+            _jumpVelocityFromCommand.Y = Mathf.Sin(Mathf.DegToRad(angle + 180));
+            _jumpVelocityFromCommand = _jumpVelocityFromCommand.Normalized() * (float)normalizedPower;
+            _lastJumpAngleFromCommand = angle;
+
+            _animatedSprite2D.Play(JumperAnimationNames.Jump);
         }
     }
 
@@ -136,9 +162,12 @@ public partial class JumperScene : CharacterBody2D
     private void HandleCharacterSetEvent()
     {
         int choice = _jumper.PlayerData.CharacterChoice;
-        string gender = choice > 9 ? "Female" : "Male";
-        int charNumber = ((choice - 1) % 9 / 3) + 1;
-        int clothingNumber = ((choice - 1) % 3) + 1;
+        int charactersCount = CharacterSpriteProvider.Instance.CharactersCount;
+        int clothingsCount = CharacterSpriteProvider.Instance.ClothingsCount;
+        int choicesPerGender = charactersCount * clothingsCount;
+        string gender = choice > choicesPerGender ? "Female" : "Male";
+        int charNumber = ((choice - 1) % choicesPerGender / charactersCount) + 1;
+        int clothingNumber = ((choice - 1) % clothingsCount) + 1;
 
         GD.Print("Choice: " + choice + " Gender: " + gender + " Char: " + charNumber + " Clothing: " + clothingNumber);
 
@@ -148,9 +177,10 @@ public partial class JumperScene : CharacterBody2D
             clothingNumber
         );
 
+        // Force playing the Idle animation when we change our character while on the floor.
         if (IsOnFloor())
         {
-            _animatedSprite2D.Play(JumperAnimations.IDLE.ToString().ToLower());
+            _animatedSprite2D.Play(JumperAnimationNames.Idle);
         }
     }
 
@@ -176,13 +206,14 @@ public partial class JumperScene : CharacterBody2D
         _glow.Emitting = true;
     }
 
+    /// <summary>
+    /// Basically the gravity application before any other component affects the velocity.
+    /// </summary>
     private void ApplyInitialVelocity(double delta)
     {
-        Vector2 velocity = Velocity;
+        float newY = IsOnFloor() ? 0 : GameConstants.Gravity * (float)delta;
 
-        velocity.Y += IsOnFloor() ? 0 : GameConstants.Gravity * (float)delta;
-
-        Velocity = velocity;
+        Velocity = new(Velocity.X, Velocity.Y + newY);
     }
 
     /// <summary>
@@ -190,10 +221,45 @@ public partial class JumperScene : CharacterBody2D
     /// </summary>
     private void ApplyJumpVelocity()
     {
-        Velocity = _jumpVelocity != Vector2.Zero ? _jumpVelocity : Velocity;
-        _jumpVelocity = Vector2.Zero;
+        Velocity = !_jumpVelocityFromCommand.IsEqualApprox(Vector2.Zero) ? _jumpVelocityFromCommand : Velocity;
+
+        // Flip the sprite base on our x velocity, but only if we recently jumped at a non-zero angle
+        if (!Mathf.IsZeroApprox(Velocity.X) && !HasRecentlyJumpedAtZeroAngle)
+        {
+            _animatedSprite2D.FlipH = Velocity.X < 0;
+        }
+
+        // Reset the jump velocity to indicate that we should not continuously apply the calculated velocity
+        _jumpVelocityFromCommand = Vector2.Zero;
     }
 
+    /// <summary>
+    /// Causes the character to rotate based on its X velocity, but only at non-zero angles.
+    /// </summary>
+    private void RotateInAir(double delta)
+    {
+        // We don't want to rotate if we just jumped straight up
+        if (HasRecentlyJumpedAtZeroAngle)
+        {
+            // Edge case reset when we jump at the very moment we hit the floor and we keep the previous rotation
+            _animatedSprite2D.RotationDegrees = 0;
+
+            return;
+        }
+
+        // Maximum velocity is 700, so we can define 10 levels of the rotation speed based on that. It won't be exactly
+        // 10, but it will be good enough to make the rotation switch only after certain amount
+        int rotationSpeedFromVelocity = Math.Clamp(Math.Abs((int)Velocity.X) / 70, 1, 10);
+
+        // Calculate the rotation factor and flip the value if we are going left (rotating in the right direction)
+        float rotationFactor = 250 * (float)delta * (Velocity.X < 0 ? -1 : 1) * rotationSpeedFromVelocity;
+
+        _animatedSprite2D.RotationDegrees = IsOnFloor() ? 0 : _animatedSprite2D.RotationDegrees + rotationFactor;
+    }
+
+    /// <summary>
+    /// Stops this body completely once it touches the floor.
+    /// </summary>
     private void StopIfOnFloor()
     {
         if (IsOnFloor())
@@ -202,11 +268,37 @@ public partial class JumperScene : CharacterBody2D
         }
     }
 
+    /// <summary>
+    /// Reflects some of the X velocity when touching the wall to achieve the "bounce" effect.
+    /// </summary>
     private void BounceOffWall()
     {
         if (IsOnWall())
         {
-            Velocity = new Vector2(_previousXVelocity * -0.5f, Velocity.Y);
+            Velocity = new Vector2(_xVelocityLastFrame * -0.5f, Velocity.Y);
+        }
+    }
+
+    /// <summary>
+    /// Switches the character animations in certain conditions when in the air. Handles falling/landing.
+    /// </summary>
+    private void PlayNotGroundedAnimation()
+    {
+        if (Velocity.Y < 0)
+        {
+            _animatedSprite2D.Play(JumperAnimationNames.Fall);
+        }
+
+        // Defines a situation when we stopped, but the animation is still playing the Jump/Fall frames
+        bool hasLandedButStillAnimating =
+            (
+                _animatedSprite2D.Animation == JumperAnimationNames.Fall
+                || _animatedSprite2D.Animation == JumperAnimationNames.Jump
+            ) && Velocity.IsEqualApprox(Vector2.Zero);
+
+        if (hasLandedButStillAnimating)
+        {
+            _animatedSprite2D.Play(JumperAnimationNames.Land);
         }
     }
 
