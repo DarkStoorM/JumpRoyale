7fe8e2c8568d4aebe106bf3d8e340810bffb9525
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-04-03 20:34:31 +0200

    Add initial tests for Timer
    This is most likely wrong, but it's just temporary anyway

---
+++ Tests/Game/TimerTests.cs
@@ -0,0 +1,52 @@
+#pragma warning disable CA2000
+
+using JumpRoyale;
+using JumpRoyale.Events;
+
+namespace Tests.Game;
+
+[TestFixture]
+public class TimerTests
+{
+    private bool _triggeredTick;
+    private bool _triggeredFinish;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _triggeredTick = false;
+        _triggeredFinish = false;
+    }
+
+    [Test]
+    public async Task CanEmitEventAtInterval()
+    {
+        EventTimer timer = new(1, 1);
+        timer.OnCheckpointReached += Tick;
+
+        await timer.Start().ConfigureAwait(false);
+
+        Assert.That(_triggeredTick, Is.True);
+    }
+
+    [Test]
+    public async Task CanEmitFinishEvent()
+    {
+        EventTimer timer = new(1, 1);
+        timer.OnFinished += Finish;
+
+        await timer.Start().ConfigureAwait(false);
+
+        Assert.That(_triggeredFinish, Is.True);
+    }
+
+    private void Tick(object sender, TimerEventArgs args)
+    {
+        _triggeredTick = true;
+    }
+
+    private void Finish(object sender, EventArgs args)
+    {
+        _triggeredFinish = true;
+    }
+}
