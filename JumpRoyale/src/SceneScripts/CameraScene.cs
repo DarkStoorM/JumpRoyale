77a38f220288065143bc95050e8529809b948931
darkstoorm <b.sieprawski@op.pl>
2024-04-06 14:34:04 +0200

    Finish camera scene handling through timers

--- JumpRoyale/src/SceneScripts/CameraScene.cs
+++ JumpRoyale/src/SceneScripts/CameraScene.cs
@@ -6,18 +6,31 @@ namespace JumpRoyale;
 
 public partial class CameraScene : Node2D
 {
-    private readonly EventTimer _timer = new(GameConstants.GameTimeInSeconds, GameConstants.ScrollSpeedChangeInterval);
+    /// <summary>
+    /// Timer started after the lobby countdown, raises an event at interval.
+    /// </summary>
+    private readonly EventTimer _gameTimer =
+        new(GameConstants.GameTimeInSeconds, GameConstants.ScrollSpeedChangeInterval);
 
+    /// <summary>
+    /// Lobby timer acting as an awaiting time, which starts the game after the countdown.
+    /// </summary>
+    private readonly EventTimer _lobbyTimer = new(GameConstants.LobbyAwaitingTime);
+
+    /// <summary>
+    /// Defines the current camera movement speed multiplier, increased at interval by game timer.
+    /// </summary>
     private int _movementMultiplier = 1;
 
-    public bool CanMove { get; set; } = true;
+    public bool CanMove { get; set; }
 
     public override void _Ready()
     {
-        _timer.OnInterval += IncreaseMovementMultiplier;
-        _timer.OnFinished += StopCamera;
+        _gameTimer.OnInterval += IncreaseMovementMultiplier;
+        _gameTimer.OnFinished += StopCamera;
+        _lobbyTimer.OnFinished += StartCamera;
 
-        _ = _timer.Start();
+        _ = _lobbyTimer.Start();
     }
 
     public override void _Process(double delta)
@@ -31,7 +44,29 @@ public partial class CameraScene : Node2D
         }
     }
 
-    private void IncreaseMovementMultiplier(object sender, EventTimerEventArgs args) => _movementMultiplier++;
+    /// <summary>
+    /// Event handler called when the game timer raises an event at set interval - makes the camera scroll faster.
+    /// </summary>
+    private void IncreaseMovementMultiplier(object sender, EventTimerEventArgs args)
+    {
+        _movementMultiplier++;
+    }
 
-    private void StopCamera(object sender, EventArgs args) => CanMove = false;
+    /// <summary>
+    /// Event handler called when the lobby timer is finished. Starts the game timer and initiates the camera scroll.
+    /// </summary>
+    private void StartCamera(object sender, EventArgs args)
+    {
+        CanMove = true;
+
+        _ = _gameTimer.Start();
+    }
+
+    /// <summary>
+    /// Event handler called when the game timer is finished. Prevents the camera from moving.
+    /// </summary>
+    private void StopCamera(object sender, EventArgs args)
+    {
+        CanMove = false;
+    }
 }
