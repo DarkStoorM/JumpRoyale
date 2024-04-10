dd4010ce977b9adb650469970dd503c0936c0f85
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-04-10 20:55:23 +0200

    Add initial Stats Scene script

---
+++ JumpRoyale/src/SceneScripts/StatsOverlayScene.cs
@@ -0,0 +1,46 @@
+using Godot;
+using JumpRoyale.Events;
+
+namespace JumpRoyale;
+
+public partial class StatsOverlayScene : Control
+{
+    /// <summary>
+    /// Label displaying the live player count, updated on join.
+    /// </summary>
+    private Label _playerCount = null!;
+
+    /// <summary>
+    /// Label displaying the current maximum height achieved by someone.
+    /// </summary>
+    private Label _currentMaxHeight = null!;
+
+    /// <summary>
+    /// Label displaying the name of the current leader (highest jumper).
+    /// </summary>
+    private Label _highestPlayer = null!;
+
+    /// <summary>
+    /// Label displaying the game timer (this component is not related to the lobby timer).
+    /// </summary>
+    private Label _gameTimer = null!;
+
+    public override void _Ready()
+    {
+        _playerCount = GetNode<Label>("PlayerCount");
+        _currentMaxHeight = GetNode<Label>("CurrentMaxHeight");
+        _highestPlayer = GetNode<Label>("HighestPlayer");
+        _gameTimer = GetNode<Label>("GameTimer");
+
+        PlayerStats.Instance.OnPlayerJoin += PollPlayerCountEvent;
+    }
+
+    /// <summary>
+    /// Retrieves the current player count from <c>PlayerStats</c>, updated when PlayerStats raises an event on new
+    /// player join.
+    /// </summary>
+    private void PollPlayerCountEvent(object sender, PlayerJoinEventArgs args)
+    {
+        _playerCount.Text = PlayerStats.Instance.JumpersCount().ToString();
+    }
+}
